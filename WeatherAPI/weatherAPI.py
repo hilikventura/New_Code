f14fd300f0a836a8b3c7daaf369e057721929c3b
AntonP <77158999+AntonPas@users.noreply.github.com>
2022-07-17 20:09:55 +0300

    Update weatherAPI.py

--- WeatherAPI/weatherAPI.py
+++ WeatherAPI/weatherAPI.py
@@ -1,17 +1,21 @@
-import requests,sys
+import requests, sys
 from flask import Flask
 from flask import request
 from flask import render_template
+import random
 
 web = Flask(__name__)
 token = "a69377a15953c8ba9671e9b62b065644"
-data="<h1>ERROR</h1>"
+data = "<h1>ERROR</h1>"
+is_random = True
+
 
 def get_weather_data(_lat, _lon, _token):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={_lat}&lon={_lon}&appid={_token}&units=metric"
     r = requests.get(url)
     data = r.json()
-    return data['name'], str(data['weather'][0]['description']).title(), data['main']['temp']
+    country = data['sys']['country'] if 'country' in data['sys'].keys() else ""
+    return data['name'], country, str(data['weather'][0]['description']).title(), data['main']['temp']
 
 
 def get_air_pollution_data(_lat, _lon, _token):
@@ -34,27 +38,35 @@ def get_cood_from_user():
 @web.route("/")
 def output():
     global data
+    if is_random:
+        main(str(random.randint(-90, 90)), str(random.randint(-180, 180)))
     return data
 
-    #return  render_template("index.html")
-
+    # return  render_template("index.html")
 
 
 def main(lan="", lon=""):
     global data
-    if lan!="" and lan!="":
-        lan,lon=int(lan),int(lon)
+    if lan != "" and lan != "":
+        lan, lon = int(lan), int(lon)
         if -90 <= lan <= 90 and -180 <= lon <= 180:
-            #lan, lon = get_cood_from_user()
-            name, desc, temp = get_weather_data(lan, lon, token)
-            aqi=get_air_pollution_data(lan, lon, token)
-            data=f"\n<h1>Weather Data about {name}\n{desc}\nAir pollution quality: {aqi} \nThe current temp is {temp}°C</h1>"
+            # lan, lon = get_cood_from_user()
+            name, country, desc, temp = get_weather_data(lan, lon, token)
+            aqi = get_air_pollution_data(lan, lon, token)
+            data = f"""<div>
+            <h1>Weather Data about {country} {name} ({lan},{lon}):</h1>
+            <p>{desc}</p> 
+            <h3>Air pollution quality: {aqi} </h3>
+            <h3>The current temp is {temp}°C</h3>
+            </div>"""
     else:
-        data="<h1>Bad Input</h1>"
-    
+        data = "<h1>Bad Input</h1>"
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3 :
-        main(list(sys.argv)[1],list(sys.argv)[2])
-        web.run(host="0.0.0.0", port=8000)
+    if len(sys.argv) == 3:
+        main(list(sys.argv)[1], list(sys.argv)[2])
+    else:
+        is_random = True
 
+    web.run(host="0.0.0.0", port=8000)
